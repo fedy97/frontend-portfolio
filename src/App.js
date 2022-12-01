a87518ef0476ed10981725754e16d46831fb894a
fedy97 <frc.morreale@gmail.com>
2022-12-01 14:26:35 +0100

    fixing blank page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path={"/"} element={<Home/>}></Route>
                 {/*<Route path={"/admin/"} element={<Admin/>}></Route>
